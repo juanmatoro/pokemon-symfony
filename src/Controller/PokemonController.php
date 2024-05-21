31ee9a0d6ce2a2e2aa156e44a054b051b6d4c2d4
Jaime Sebastián <jaiminator21@gmail.com>
2024-05-21 20:07:32 +0200

    rutas funcionales

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -3,55 +3,33 @@
 namespace App\Controller;
 
 use App\Entity\Pokemon;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PokemonController extends AbstractController
-{
-    #[Route("/pokemon")]
-    public function showPokemon()
+{       ///pokemon/{name}
+    #[Route("/pokemon/{id}", name:"viewpokemon")]
+    public function showPokemon(EntityManagerInterface $doctrine, $id/* $name */)
     {
 
-        $pokemon = [
-            "name" => "Pikachu",
-            "description" => "Cuando se enfada, este Pokémon descarga la energía que almacena en el interior de las bolsas de las mejillas.",
-            "image" => "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png",
-            "code" => "0025"
-
-        ];
+        $repository = $doctrine->getRepository(Pokemon::class);
+        $pokemon = $repository->find($id);
+       // $pokemon = $repository->findOneBy(["name"=>$name]);
         return $this->render("pokemons/showPokemon.html.twig", ["pokemon" => $pokemon]);
     }
-    #[Route("/pokemons")]
-    public function showPokemons()
+    #[Route("/pokemons", name:"showpokemons")]
+    public function showPokemons(EntityManagerInterface $doctrine)
     {
-
-        $pokemons = [
-            [
-                "name" => "Pikachu",
-                "description" => "Cuando se enfada, este Pokémon descarga la energía que almacena en el interior de las bolsas de las mejillas.",
-                "image" => "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png",
-                "code" => "0025"
-            ],
-            [
-                "name" => "Drakloak",
-                "description" => "Vuela a 200 km/h. Si pierde un combate, el Dreepy que lo acompaña no dudará un segundo en emprender la huida.",
-                "image" => "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/886.png",
-                "code" => "0886"
-            ],
-            [
-                "name" => "0001",
-                "description" => "Tras nacer, crece alimentándose durante un tiempo de los nutrientes que contiene el bulbo de su lomo.",
-                "image" => "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png",
-                "code" => "0001"
-            ],
-
-        ];
+        $repository = $doctrine->getRepository(Pokemon::class);
+        $pokemons = $repository->findAll();
         return $this->render("pokemons/showPokemons.html.twig", ["pokemons" => $pokemons]);
     }
 
-    #[Route("/insert/pokemon")]
+    #[Route("/insert/pokemon", name:"newpokemon")]
     public function insertPokemon(EntityManagerInterface $doctrine)
     {
         $pokemon = new Pokemon();
